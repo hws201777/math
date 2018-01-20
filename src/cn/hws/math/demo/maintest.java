874943d003bd5cfdcbfa1b378b177d82a4a6e0fa
hws201777 <hwsh77@163.com>
2018-01-21 00:48:52 +0800

    222

--- src/cn/hws/math/demo/maintest.java
+++ src/cn/hws/math/demo/maintest.java
@@ -2,6 +2,9 @@ package cn.hws.math.demo;
 
 import org.junit.Test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -34,6 +37,39 @@ public class maintest {
 
 
 
+    }
+    @Test
+    public void aaa(){
+        Map map = new HashMap<>();
+        setTime(map,14);
+        System.out.print(map);
+    }
+
+    private void setTime(Map map,int val) {
+        try {
+
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+            SimpleDateFormat format2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Calendar cal = Calendar.getInstance();
+            int day_of_week = cal.get(Calendar.DAY_OF_WEEK) - 2;
+            cal.add(Calendar.DATE, -(day_of_week+val));
+            String startTime = format.format(cal.getTime());
+            map.put("startTime",format2.parse(startTime+" 00:00:00"));
+            map.put("startTimeLimit",format2.parse(startTime+" 00:00:00"));
+            cal.add(Calendar.DATE, 6);
+            String endTime = format.format(cal.getTime());
+            map.put("endTime",format2.parse(endTime+" 23:59:59"));
+            map.put("endTimeLimit",format2.parse(endTime+" 23:59:59"));
+            if(val==0){
+                map.remove("endTime");
+                map.remove("startTime");
+            }else{
+                map.remove("startTimeLimit");
+                map.remove("endTimeLimit");
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
 }
